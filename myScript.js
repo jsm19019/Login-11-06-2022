49e5137d99b504291746e4a84e00e17d128b8539
jsm19019 <106869414+jsm19019@users.noreply.github.com>
2022-06-11 17:54:38 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,86 @@
+function loginfunc(){
+    document.getElementById('loginpage').style.display="block"
+    document.getElementById('signuppage').style.display="none"
+    document.getElementById('gallery1').style.display="none"
+    document.getElementById('spring').style.display="none"
+    document.getElementById('tagline').style.display="none"
+    document.getElementById('logsine').style.display="none"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('about').style.display="none"
+}
+function loginclose(){
+    document.getElementById('loginpage').style.display="none"
+    document.getElementById('tagline').style.display="block"
+    document.getElementById('logsine').style.display="block"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('about').style.display="none"
+    
+}
+function signupfunc(){
+    document.getElementById('signuppage').style.display="block"
+    document.getElementById('loginpage').style.display="none"  
+    document.getElementById('gallery1').style.display="none"
+    document.getElementById('spring').style.display="none"
+    document.getElementById('tagline').style.display="none"
+    document.getElementById('logsine').style.display="none"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('about').style.display="none"
+}
+function signupclose(){
+    document.getElementById('signuppage').style.display="none"
+    document.getElementById('tagline').style.display="block"
+    document.getElementById('logsine').style.display="block"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('about').style.display="none"
+
+}
+function gallery(){
+    document.getElementById('gallery1').style.display="block"
+    document.getElementById('signuppage').style.display="none"
+    document.getElementById('loginpage').style.display="none"
+    document.getElementById('spring').style.display="none"
+    document.getElementById('tagline').style.display="none"
+    document.getElementById('logsine').style.display="none"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('about').style.display="none"
+}
+function springfunc(){
+document.getElementById('spring').style.display="block"
+document.getElementById('gallery1').style.display="none"
+document.getElementById('signuppage').style.display="none"
+document.getElementById('loginpage').style.display="none"
+document.getElementById('tagline').style.display="none"
+document.getElementById('logsine').style.display="none"
+document.getElementById('contact').style.display="none"
+document.getElementById('about').style.display="none"
+}
+function home(){
+document.getElementById('tagline').style.display="block"
+document.getElementById('spring').style.display="none"
+document.getElementById('gallery1').style.display="none"
+document.getElementById('signuppage').style.display="none"
+document.getElementById('loginpage').style.display="none"
+document.getElementById('logsine').style.display="block"
+document.getElementById('contact').style.display="none"
+document.getElementById('about').style.display="none"
+}
+function contact(){
+    document.getElementById('contact').style.display="block"
+    document.getElementById('tagline').style.display="none"
+    document.getElementById('logsine').style.display="none"
+    document.getElementById('gallery1').style.display="none"
+    document.getElementById('spring').style.display="none"
+    document.getElementById('loginpage').style.display="none"
+    document.getElementById('signuppage').style.display="none"
+    document.getElementById('about').style.display="none"
+}
+function about(){
+    document.getElementById('about').style.display="block"
+    document.getElementById('contact').style.display="none"
+    document.getElementById('tagline').style.display="none"
+    document.getElementById('logsine').style.display="none"
+    document.getElementById('gallery1').style.display="none"
+    document.getElementById('spring').style.display="none"
+    document.getElementById('loginpage').style.display="none"
+    document.getElementById('signuppage').style.display="none"
+}
